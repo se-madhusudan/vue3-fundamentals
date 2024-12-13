98bd07c8b87fc15f7be743c6fb670679644edd3e
Dharminder7 <madhusudan.kumar@tmotions.com>
2024-12-13 15:54:53 +0530

    added header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,9 +1,80 @@
 <template>
-    
+    <header class="header">
+        <div class="header__logo">
+            <img :src="logoDetails.logoUrl" :alt="logoDetails.logoCaption" />
+        </div>
+        <nav class="header__nav">
+            <ul>
+                <li v-for="(link, index) in links" :key="index" >
+                    <a :href="link.url">{{ link.text }}</a>
+                </li>
+            </ul>
+        </nav>
+    </header>
 </template>
 
 <script>
 export default {
-    
+    props: {
+        links: {
+            type: Array,
+            Required: true
+        },
+        logoDetails: {
+            type: Object,
+            Required: true
+        }
+    }
 }
 </script>
+
+<style lang="scss" scoped>
+.header {
+    width: 100%;
+    height: 70px;
+    display: flex;
+    padding: 10px;
+    margin: 0;
+    background-color: #f3e0d1;
+    justify-content: space-between;
+    align-items: center;
+    
+    &__logo {
+        width: 50px;
+        height: 50px;
+
+        img {
+            width: 100%;
+            object-fit: cover;
+        }
+    }
+
+    &__nav {
+        width: 70%;
+
+        ul {
+            display: flex;
+            width: 100%;
+            list-style-type: none;
+            justify-content: flex-end;
+            gap: 30px;
+            padding: 0;
+
+            li {
+                a {
+                    text-decoration: none;
+                    font-size: 18px;
+                    font-weight: 700;
+                    font-family:'Lucida Sans', Geneva, Verdana, sans-serif;
+                    color: #f13d10;
+                }
+
+                a:hover {
+                    color: #0074a1;
+                }
+            }
+        }
+    }
+
+}
+</style>
