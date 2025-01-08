4204b528cd1296f2fd7bd1cc56040bd06e03d97d
Dharminder7 <madhusudan.kumar@tmotions.com>
2025-01-08 15:24:05 +0530

    base updated

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/vue3-fundamentals/',
+  base: 'vue3-fundamentals',
   plugins: [
     vue(),
     vueDevTools(),
