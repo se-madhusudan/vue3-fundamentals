60c57213a1cc96c8012ee8b9d101433856806542
Dharminder7 <madhusudan.kumar@tmotions.com>
2025-01-08 16:09:17 +0530

    Set up GitHub Actions for deployment

--- vite.config.js
+++ vite.config.js
@@ -6,10 +6,8 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  base: '/vue3-fundamentals/',
+  plugins: [vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
